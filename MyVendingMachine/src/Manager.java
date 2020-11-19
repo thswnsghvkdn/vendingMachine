381ee70a4aa96336989f1f0c22635d0c10556f80
DESKTOP-M8IQ2EL\xxeun <ths3630802@naver.com>
2020-11-19 21:17:42 +0900

    서버 개설
    서버를 통하여 재고관리를 하기위해 소켓을 연결하였다.

---
+++ MyVendingMachine/src/Manager.java
@@ -0,0 +1,49 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+
+
+
+public class Manager extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Manager frame = new Manager();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Manager() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 534, 491);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(51, 61, 333, 333);
+		contentPane.add(textField);
+		textField.setColumns(10);
+	}
+}
